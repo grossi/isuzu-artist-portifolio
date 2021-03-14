059464b7346d83915d2e25aa15f66ffdfe3fd6fd
Gabriel Rossi <gabriel.rossi-ext@viavarejo.com.br>
2021-03-14 11:27:59 -0300

    style: Minor changes in style, adds todo comment on header

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -2,11 +2,10 @@ import * as React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
-
-import PhotoGallery from "react-photo-gallery";
-import Grid from "@material-ui/core/Grid";
-import Dialog from "@material-ui/core/Dialog";
 import CardMedia from "@material-ui/core/CardMedia";
+import Dialog from "@material-ui/core/Dialog";
+import Grid from "@material-ui/core/Grid";
+import PhotoGallery from "react-photo-gallery";
 import galleryData from "gallery.json";
 
 const useStyles = makeStyles((theme) => ({
@@ -17,7 +16,6 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.grey[200],
   },
 }));
-
 
 const paintings = galleryData.map((image, k) => {
   return {

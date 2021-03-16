1f3945ed5f802192bd9280ef5a96e4ad08fe308e
Gabriel Rossi <gabriel.rossi-ext@viavarejo.com.br>
2021-03-16 20:37:33 -0300

    feat: First attempt at lazy loading. Layout breaking, needs work.

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,19 +1,26 @@
 import * as React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
+import { makeStyles } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 import CardMedia from "@material-ui/core/CardMedia";
 import Dialog from "@material-ui/core/Dialog";
 import Grid from "@material-ui/core/Grid";
-import PhotoGallery from "react-photo-gallery";
+import PhotoGallery, { RenderImageProps } from "react-photo-gallery";
+import LazyLoad from "react-lazyload";
 import galleryData from "gallery.json";
 
 const useStyles = makeStyles((theme) => ({
   closeButton: {
-    position: 'absolute',
+    position: "absolute",
     right: theme.spacing(1),
     top: theme.spacing(1),
     color: theme.palette.grey[200],
+  },
+  image: {
+    cursor: "pointer",
+    overflow: "hidden",
+    position: "relative",
+    width: "auto",
   },
 }));
 
@@ -22,7 +29,7 @@ const paintings = galleryData.map((image, k) => {
     src: `${process.env.PUBLIC_URL}/${image.location}`,
     width: image.width,
     height: image.height,
-    name: image.name,
+    alt: image.name,
   };
 });
 
@@ -39,6 +46,25 @@ export default function Gallery() {
   const [currentImage, setCurrentImage] = React.useState("");
   const [viewerIsOpen, setViewerIsOpen] = React.useState(false);
 
+  const LazyImageLoading = React.useCallback((props: RenderImageProps) => {
+    return (
+      <LazyLoad height={props.photo.height} style={{ margin: `${props.margin}px`}}>
+        <img
+          className={classes.image}
+          height={props.photo.height}
+          width={props.photo.width}
+          onClick={(event: React.MouseEvent<Element, MouseEvent>) =>
+            props.onClick
+              ? props.onClick(event, { ...props.photo, index: props.index })
+              : {}
+          }
+          src={props.photo.src}
+          alt={props.photo.alt}
+        />
+      </LazyLoad>
+    );
+  }, [classes.image]);
+
   const openDialog = React.useCallback((event, { photo, index }) => {
     setCurrentImage(photo.src);
     setViewerIsOpen(true);
@@ -52,6 +78,7 @@ export default function Gallery() {
           photos={paintings}
           onClick={openDialog}
           limitNodeSearch={4}
+          renderImage={LazyImageLoading}
         />
       </Grid>
       <Dialog
@@ -61,7 +88,11 @@ export default function Gallery() {
         maxWidth="xl"
       >
         <CardMedia component="img" src={currentImage} />
-        <IconButton aria-label="close" className={classes.closeButton} onClick={() => setViewerIsOpen(false)}>
+        <IconButton
+          aria-label="close"
+          className={classes.closeButton}
+          onClick={() => setViewerIsOpen(false)}
+        >
           <CloseIcon />
         </IconButton>
       </Dialog>
